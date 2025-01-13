3c200443c18a1b8848a17d44f582bc136e47af17
Harsh Soni <harsh@pseudocode.co>
2025-01-13 16:36:33 +0530

    Integrated the basic openlayer map with the particular position to india

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/tile';
+import { fromLonLat } from 'ol/proj';
+import { OSM } from 'ol/source';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrl: './map.component.scss',
 })
-export class MapComponent {}
+export class MapComponent implements OnInit {
+  map!: Map;
+
+  ngOnInit(): void {
+    const initialLonLat = [78.9629, 20.5937];
+    const initialWebMercator = fromLonLat(initialLonLat);
+    this.map = new Map({
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      target: 'map',
+      view: new View({
+        center: initialWebMercator,
+        zoom: 4,
+      }),
+    });
+  }
+}
