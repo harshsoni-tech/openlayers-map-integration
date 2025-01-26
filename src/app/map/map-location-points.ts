5fa3abe8bb7b7b27fc99a21b763493a0192ca1ef
Harsh Soni <harsh@pseudocode.co>
2025-01-27 00:48:47 +0530

    completed the project integrated openlayers in angular 18 and added initial marker when we click anywhere on the map it will directly add the marker at that place

---
+++ src/app/map/map-location-points.ts
@@ -0,0 +1,18 @@
+export const locationPoints = [
+  {
+    latitude: 28.617783046544073,
+    longitude: 77.18890893489043,
+  },
+  {
+    latitude: 23.05383955880682,
+    longitude: 72.54028008874279,
+  },
+  {
+    latitude: 23.101917412124546,
+    longitude: 86.72889106548904,
+  },
+  {
+    latitude: 19.040131274165514,
+    longitude: 72.90003125854125,
+  },
+];
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,9 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import Map from 'ol/Map';
 import View from 'ol/View';
-import TileLayer from 'ol/layer/tile';
-import { fromLonLat } from 'ol/proj';
-import { OSM } from 'ol/source';
+import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
+import OSM from 'ol/source/OSM';
+import VectorSource from 'ol/source/Vector';
+import GeoJSON from 'ol/format/GeoJSON';
+import { Fill, Icon, Style } from 'ol/style';
+import { fromLonLat, toLonLat } from 'ol/proj';
+import { Feature } from 'ol';
+import { Point, Polygon } from 'ol/geom';
+import { locationPoints } from './map-location-points';
 
 @Component({
   selector: 'app-map',
@@ -12,21 +18,66 @@ import { OSM } from 'ol/source';
 })
 export class MapComponent implements OnInit {
   map!: Map;
+  vectorSource!: VectorSource;
+  vectorLayer!: VectorLayer;
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.initializeMap();
+  }
+
+  initializeMap() {
     const initialLonLat = [78.9629, 20.5937];
-    const initialWebMercator = fromLonLat(initialLonLat);
+    const initialMapPosition = fromLonLat(initialLonLat);
+    this.vectorSource = new VectorSource();
+    this.vectorLayer = new VectorLayer({
+      source: this.vectorSource,
+    });
+
     this.map = new Map({
+      target: 'map',
       layers: [
         new TileLayer({
           source: new OSM(),
         }),
+        this.vectorLayer,
       ],
-      target: 'map',
       view: new View({
-        center: initialWebMercator,
-        zoom: 4,
+        center: initialMapPosition,
+        zoom: 5,
       }),
     });
+    locationPoints.forEach((coordinate) => {});
+    this.addMarker();
+    this.map.on('click', (evt) => {
+      const lonLat = toLonLat(evt.coordinate);
+      locationPoints.push({
+        latitude: lonLat[1],
+        longitude: lonLat[0],
+      });
+      this.addMarker();
+    });
   }
+
+  addMarker() {
+    this.vectorSource.clear();
+    locationPoints.forEach((point: { latitude: number; longitude: number }) => {
+      const marker = new Feature({
+        geometry: new Point(fromLonLat([point.longitude, point.latitude])),
+      });
+      marker.setStyle(
+        new Style({
+          image: new Icon({
+            anchor: [0.5, 0.5],
+            src: 'map-icons/location-icon.png',
+            scale: 0.1,
+          }),
+        })
+      );
+      this.vectorSource.addFeature(marker);
+    });
+  }
+
+  addMarkerOnMapClick() {}
 }
